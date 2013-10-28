70adcb3e903380d2a61009e5588362aa98b513b8
Henrique Ferrolho <henriqueferrolho@gmail.com>
2013-10-28 00:34:56 +0000

    Ferrolho: simplified A - Speed Leaks

--- MIUP/2013/A - Speed Leaks/henriqueferrolho.cpp
+++ MIUP/2013/A - Speed Leaks/henriqueferrolho.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <iostream>
 #include <string>	
 #include <vector>
 #include <map>
@@ -8,53 +7,38 @@ using namespace std;
 typedef vector<int> vi;
 typedef vector<unsigned int> vui;
 #define FOR(i, a, b) for(unsigned int i = a; i < b; i++)
-
+#define ALL(v) (v).begin(), (v).end()
+#define foreach(v, it) for(typeof((v).begin()) it = (v).begin(); it != (v).end(); it++ )
+ 
 struct Record {
-        string plate;
-        vui speeds;
+	string plate;
+	vui speeds;
+
+	bool operator <(const Record &r) const {
+		if (speeds != r.speeds)
+			return speeds > r.speeds;
+		return plate < r.plate;
+	}
 };
 
-bool faster(unsigned int i, unsigned int j) { return (i > j); }
+bool faster(unsigned int i, unsigned int j) { return i > j; }
 
 void const printRecords(vector<Record> &records) {
-        FOR(i, 0, records.size()) {
-                // printing plate
-                printf("%s", records[i].plate.c_str());
+    FOR(i, 0, records.size()) {
+        // printing plate
+        printf("%s", records[i].plate.c_str());
 
-                // printing speeds
-                FOR(j, 0, records[i].speeds.size()) {
-                        printf(" %u", records[i].speeds[j]);
-                }
-                printf("\n");
-        }
-}
-
-bool const biggerRecord(Record r1, Record r2) {
-	unsigned int limit = r1.speeds.size();
-
-	if (r2.speeds < r1.speeds)
-		limit = r2.speeds.size();
-
-	FOR(i, 0, limit) {
-		if (r1.speeds[i] > r2.speeds[i])
-			return true;
-		else if (r1.speeds[i] < r2.speeds[i])
-			return false;
-	}
-
-	// if all values are equal but one vec is bigger than other
-	if (r1.speeds > r2.speeds)
-		return true;
-	else if (r1.speeds < r2.speeds)
-		return false;
-	// if speed records are the same, compare plate
-	else
-		return r1.plate < r2.plate;
+        // printing speeds
+        FOR(j, 0, records[i].speeds.size())
+        	printf(" %u", records[i].speeds[j]);
+        printf("\n");
+    }
 }
 
 int main() {
 	// initializing data
 	map<string, vui> records;
+	vector<Record> sortedRecords;
 
 	// reading input
 	char plate[12];
@@ -62,13 +46,10 @@ int main() {
 	while(scanf("%s %u", plate, &speed) != EOF)
 		records[string(plate)].push_back(speed);
 
-	// sorting speeds for each record
-	for(map<string, vui>::iterator it = records.begin(); it != records.end(); it++)
-		sort(it->second.begin(), it->second.end(), faster);
+	// sorting speeds for each record and placing data in a vector
+	foreach(records, it) {
+		sort(ALL(it->second), faster);
 
-	// switching to vector to sort data
-	vector<Record> sortedRecords;
-	for(map<string, vui>::iterator it = records.begin(); it != records.end(); it++) {
 		Record temp;
 		temp.plate = it->first;
 		temp.speeds = it->second;
@@ -76,7 +57,7 @@ int main() {
 	}
 
 	// sorting records
-	sort(sortedRecords.begin(), sortedRecords.end(), biggerRecord);
+	sort(ALL(sortedRecords));
 
 	// outputting data
 	printRecords(sortedRecords);
