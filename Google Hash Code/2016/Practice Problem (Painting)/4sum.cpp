c709f553385f10619df6d7a786c3cff01358ff37
ferrolho <henriqueferrolho@gmail.com>
2016-02-11 15:28:31 +0000

    reverting problem

--- Google Hash Code/2016/Practice Problem (Painting)/4sum.cpp
+++ Google Hash Code/2016/Practice Problem (Painting)/4sum.cpp
@@ -13,35 +13,62 @@ typedef vector<vector<unsigned int>> vvui;
 // Macros
 #define ALL(v) (v).begin(), (v).end()
 
+// Constants
+const ui N = 1000;
+const ui M = 1000;
+
+// Globals
+bool picture[N][M];
+bool visited[N][M];
+
 // Classes
 class Instruction {
+
+	virtual void markAsVisited() = 0;
+
 	virtual std::ostream& print(std::ostream&) const = 0;
+
 public:
+
 	virtual ~Instruction() {
 	}
 
 	friend ostream& operator<<(ostream& os, const Instruction& i) {
 		return i.print(os);
 	}
+
 };
 
-class Square : public Instruction {
+class Square: public Instruction {
+
 	ui r, c, s;
+
 public:
+
 	Square(ui r, ui c, ui s) {
 		this->r = r;
 		this->c = c;
 		this->s = s;
 	}
 
+	void markAsVisited() {
+		for (ui i = r - s; i <= r + s; i++)
+			for (ui j = c - s; j <= c + s; j++)
+				visited[i][j] = true;
+	}
+
 	std::ostream& print(std::ostream& os) const {
 		return os << "PAINT_SQUARE " << r << " " << c << " " << s;
 	}
+
 };
 
-class Line : public Instruction {
+class Line: public Instruction {
+
 	ui r1, c1, r2, c2;
+
 public:
+
 	Line(ui r1, ui c1, ui r2, ui c2) {
 		this->r1 = r1;
 		this->c1 = c1;
@@ -50,32 +77,23 @@ public:
 		this->c2 = c2;
 	}
 
+	void markAsVisited() {
+		if (c1 == c2) {
+			// vertical line
+			for (ui i = r1; i <= r2; i++)
+				visited[i][c1] = true;
+		} else {
+			// horizontal line
+			for (ui i = c1; i <= c2; i++)
+				visited[r1][i] = true;
+		}
+	}
+
 	std::ostream& print(std::ostream& os) const {
 		return os << "PAINT_LINE " << r1 << " " << c1 << " " << r2 << " " << c2;
 	}
+
 };
-
-class Erase : public Instruction {
-	ui r, c;
-public:
-	Erase(ui r, ui c) {
-		this->r = r;
-		this->c = c;
-	}
-
-	std::ostream& print(std::ostream& os) const {
-		return os << "ERASE_CELL " << r << " " << c;
-	}
-};
-
-// Constants
-const ui N = 1000;
-const ui M = 1000;
-
-// Globals
-bool picture[N][M];
-bool visited[N][M];
-vector<Instruction*> instructions;
 
 int main() {
 	/*
@@ -95,78 +113,47 @@ int main() {
 	}
 
 	/*
-	 * Processing NxN squares with N-2 empty cells
+	 * Processing
 	 */
-
-	for (ui i = 1; i < n; i++) {
-		for (ui j = 1; j < m; j++) {
-			if (picture[i][j] && !visited[i][j]) {
-				ui n = 0;
-				vector<pair<ui, ui>> emptyCells = vector<pair<ui, ui>>();
-				bool flag = false, make = false;
-				while (n++ && emptyCells.size() <= n) {
-					if (flag) {
-						visited[i + n - 1][j] = true;
-						visited[i - n - 1][j] = true;
-						visited[i + n - 1][j + n - 1] = true;
-						visited[i - n - 1][j - n - 1] = true;
-						visited[i + n - 1][j - n - 1] = true;
-						visited[i - n - 1][j - n - 1] = true;
-						visited[i][j + n - 1] = true;
-						visited[i][j - n - 1] = true;
-						make = true;
-					}
-					if (!picture[i + n][j]) {
-						emptyCells.push_back(make_pair(i + n, j));
-					}
-					if (!picture[i - n][j]) {
-						emptyCells.push_back(make_pair(i - n, j));
-					}
-					if (!picture[i + n][j - n]) {
-						emptyCells.push_back(make_pair(i + n, j - n));
-					}
-					if (!picture[i + n][j + n]) {
-						emptyCells.push_back(make_pair(i + n, j + n));
-					}
-					if (!picture[i - n][j + n]) {
-						emptyCells.push_back(make_pair(i - n, j + n));
-					}
-					if (!picture[i - n][j - n]) {
-						emptyCells.push_back(make_pair(i - n, j - n));
-					}
-					if (!picture[i][j + n]) {
-						emptyCells.push_back(make_pair(i, j + n));
-					}
-					if (!picture[i][j - n]) {
-						emptyCells.push_back(make_pair(i, j - n));
-					}
-					flag = true;
-				}
-
-				if (make) {
-					for (ui i = 0; i < emptyCells.size(); i++)
-						instructions.push_back(new Erase(emptyCells[i].first, emptyCells[i].second));
-					instructions.push_back(new Square(i, j, n));
-				}
-			}
-		}
-	}
-
-
-	/*
-	* Processing lines and isolated squares
-	*/
+	vector<Instruction*> instructions;
 
 	for (ui i = 0; i < n; i++)
 		for (ui j = 0; j < m; j++)
 			if (picture[i][j] && !visited[i][j]) {
-				ui jStart = j;
+				// expand to the right
+				ui right = j;
+				ui horNonVisited = 0;
 
-				do {
-					j++;
-				} while (j < m && picture[i][j]);
+				while (right < m && picture[i][right]) {
+					if (!visited[i][right])
+						horNonVisited++;
 
-				instructions.push_back(new Line(i, jStart, i, j - 1));
+					right++;
+				}
+				right--;
+
+				// expand downwards
+				ui down = i;
+				ui verNonVisited = 0;
+
+				while (down < n && picture[down][j]) {
+					if (!visited[down][j])
+						verNonVisited++;
+
+					down++;
+				}
+				down--;
+
+				// save best instruction
+				Line* line =
+						horNonVisited > verNonVisited ?
+								new Line(i, j, i, right) :
+								new Line(i, j, down, j);
+
+				instructions.push_back(line);
+
+				// update visited cells matrix
+				line->markAsVisited();
 			}
 
 	/*
