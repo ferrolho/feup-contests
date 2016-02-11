837f50fdc76fe0bd01d86443000ecc6e829dcfd7
ferrolho <henriqueferrolho@gmail.com>
2016-02-11 00:50:08 +0000

    To be continued tomorrow

---
+++ Google Hash Code/2016/Practice Problem (Painting)/4sum.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Custom types
+typedef unsigned int ui;
+typedef vector<int> vi;
+typedef vector<unsigned int> vui;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<unsigned int>> vvui;
+
+// Macros
+#define ALL(v) (v).begin(), (v).end()
+
+// Classes
+class Instruction {
+	virtual std::ostream& print(std::ostream&) const = 0;
+public:
+	virtual ~Instruction() {
+	}
+
+	friend ostream& operator<<(ostream& os, const Instruction& i) {
+		return i.print(os);
+	}
+};
+
+class Square: public Instruction {
+	ui r, c, s;
+public:
+	Square(ui r, ui c, ui s) {
+		this->r = r;
+		this->c = c;
+		this->s = s;
+	}
+
+	std::ostream& print(std::ostream& os) const {
+		return os << "PAINT_SQUARE " << r << " " << c << " " << s;
+	}
+};
+
+class Line: public Instruction {
+public:
+	ui r1, c1, r2, c2;
+
+	Line(ui r1, ui c1, ui r2, ui c2) {
+		this->r1 = r1;
+		this->c1 = c1;
+
+		this->r2 = r2;
+		this->c2 = c2;
+	}
+
+	std::ostream& print(std::ostream& os) const {
+		return os << "PAINT_LINE " << r1 << " " << c1 << " " << r2 << " " << c2;
+	}
+};
+
+// Constants
+const ui N = 1000;
+const ui M = 1000;
+
+// Globals
+bool picture[N][M];
+bool visited[N][M];
+
+int main() {
+	/*
+	 * Input
+	 */
+	ui n, m;
+	cin >> n >> m;
+
+	for (ui i = 0; i < n; i++) {
+		for (ui j = 0; j < m; j++) {
+			char ch;
+
+			cin >> ch;
+
+			picture[i][j] = ch == '#';
+		}
+	}
+
+	/*
+	 * Processing
+	 */
+	vector<Instruction*> instructions;
+
+	for (ui i = 0; i < n; i++)
+		for (ui j = 0; j < m; j++)
+			if (picture[i][j] && !visited[i][j]) {
+				// expand to the right
+				ui right = j;
+				ui horNonVisited = 0;
+
+				while (right < m && picture[i][right]) {
+					if (!visited[i][right])
+						horNonVisited++;
+
+					right++;
+				}
+				right--;
+
+				// expand downwards
+				ui down = i;
+				ui verNonVisited = 0;
+
+				while (down < n && picture[down][j]) {
+					if (!visited[down][j])
+						verNonVisited++;
+
+					down++;
+				}
+				down--;
+
+				// save best instruction
+				Line* line =
+						horNonVisited > verNonVisited ?
+								new Line(i, j, i, right) :
+								new Line(i, j, down, j);
+
+				instructions.push_back(line);
+
+				// update visited cells matrix
+				if (horNonVisited > verNonVisited)
+					for (ui k = line->c1; k <= line->c2; k++)
+						visited[line->r1][k] = true;
+				else
+					for (ui k = line->r1; k <= line->r2; k++)
+						visited[k][line->c1] = true;
+			}
+
+	/*
+	 * Output
+	 */
+	cout << instructions.size() << endl;
+
+	for (auto inst : instructions)
+		cout << *inst << endl;
+
+	return 0;
+}
