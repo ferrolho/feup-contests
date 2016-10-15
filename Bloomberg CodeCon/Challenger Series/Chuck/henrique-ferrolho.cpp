82128151a3515ad63a779bbe25110a9fc69ee936
ferrolho <henriqueferrolho@gmail.com>
2016-10-15 21:24:45 +0100

    Adds Chuck

---
+++ Bloomberg CodeCon/Challenger Series/Chuck/henrique-ferrolho.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Custom types
+typedef unsigned int ui;
+
+string smallestMultipleOf4ThatBeginsWithN(ui n) {
+	auto nStr = to_string(n);
+
+	ui multiple = 4;
+	auto multipleStr = to_string(multiple);
+
+	while (multipleStr.compare(0, nStr.length(), nStr) != 0) {
+		multiple += 4;
+		multipleStr = to_string(multiple);
+	}
+
+	return multipleStr;
+}
+
+int main() {
+	ui n;
+	cin >> n;
+
+	string s;
+
+	for (ui i = 1; s.length() < n;i++)
+		s.append(smallestMultipleOf4ThatBeginsWithN(i));
+
+	cout << s[n - 1] << endl;
+
+	return 0;
+}
