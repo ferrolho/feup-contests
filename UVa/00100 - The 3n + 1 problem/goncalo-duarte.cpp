72b2aebb021c2e30799c365460b07e01e3e74cef
GDuarteL <up201303198@fe.up.pt>
2017-03-25 15:04:24 +0000

    Dynamic programming solution

---
+++ UVa/00100 - The 3n + 1 problem/goncalo-duarte.cpp
@@ -0,0 +1,26 @@
+#include <unordered_map>
+
+std::unordered_map<int, int> map;
+
+int getCycleLength(int n){
+        if(n == 1) {
+                return 1;
+        }
+        if(map.find(n) == map.end()) {
+                map[n] = getCycleLength(n % 2 == 0 ? n / 2 : 3 * n + 1) + 1;;
+        }
+        return map[n];
+}
+
+int main(int argc, char const *argv[]) {
+        int i, j;
+        while(scanf("%d %d", &i, &j) != EOF) {
+                int max = 0;
+                for(int k = i; k <= j; k++) {
+                        int length = getCycleLength(k);
+                        max = length > max ? length : max;
+                }
+                printf("%d %d %d\n", i, j, max);
+        }
+        return 0;
+}
